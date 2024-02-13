06c153fcbe7abd6f037a2b4c114bcdbfe0dd6357
JxA <85507049+AbdullahJxA@users.noreply.github.com>
2024-02-13 18:14:43 +0300

    Update invite.js

--- SlashCommands/General/invite.js
+++ SlashCommands/General/invite.js
@@ -12,7 +12,7 @@ module.exports = {
 var e = new MessageEmbed()
 .setTitle(`Thanks for inviting ${client.user.username}`)
 .setColor(`#5241b4`)
-.setThumbnail(`https://cdn.discordapp.com/avatars/1002665491590033440/7992c22fb779aad4456c117ec41fbe7c.png`)
+.setThumbnail(`${client.user.displayAvatarURL()}`)
 .setDescription(`> **Thanks for inviting me 🌹 [Click me](https://discord.com/oauth2/authorize?client_id=${client.user.id}&permissions=8&scope=bot%20applications.commands)**`)
 .setTimestamp()
 .setAuthor({name : `${interaction.guild.name}`, iconURL : `${interaction.guild.iconURL({dynamic : true}) || `https://cdn.discordapp.com/avatars/1002665491590033440/7992c22fb779aad4456c117ec41fbe7c.png`}`})
